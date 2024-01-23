8e55141d86f0505d536835da160c09bdef098357
Hulekal <sachin.hulekal@hp.com>
2024-01-23 12:55:52 +0530

    updated

--- src/main/resources/changelog.sql
+++ src/main/resources/changelog.sql
@@ -1,10 +1,10 @@
 --liquibase formatted sql
 
---changeset lumme1:6
+--changeset lumme:1
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     username text
 );
 
---change lumme2:3
+--change lumme:2
 INSERT INTO "user" (username) VALUES ('admin');
