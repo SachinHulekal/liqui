08536d9d8142116b54398c1512be6bb7e4ec3599
Hulekal <sachin.hulekal@hp.com>
2024-01-23 11:23:19 +0530

    updated

---
+++ src/main/java/liqui/sac/App.java
@@ -0,0 +1,41 @@
+package liqui.sac;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class App {
+
+  public String getCurrentTime(Context context) {
+    LambdaLogger logger = context.getLogger();
+    logger.log("Invoked JDBCSample.getCurrentTime");
+
+    String currentTime = "unavailable";
+
+    // Get time from DB server
+    try {
+      String url = "jdbc:postgresql://database-1.c9yewug28hvf.ap-south-1.rds.amazonaws.com:5434/postgres";
+      String username = "postgres";
+      String password = "postgres";
+
+      Connection conn = DriverManager.getConnection(url, username, password);
+      Statement stmt = conn.createStatement();
+      ResultSet resultSet = stmt.executeQuery("SELECT NOW()");
+
+      if (resultSet.next()) {
+        currentTime = resultSet.getObject(1).toString();
+      }
+
+      logger.log("Successfully executed query.  Result: " + currentTime);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      logger.log("Caught exception: " + e.getMessage());
+    }
+
+    return currentTime;
+  }
+}
